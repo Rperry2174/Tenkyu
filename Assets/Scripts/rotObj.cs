6885862c1ad19716eeefa1f3c7c7b63306c4dcd5
Ryan Perry <Rperry2174@gmail.com>
2018-10-14 17:39:27 -0700

    Add tenkyu clone to github

---
+++ Assets/Scripts/rotObj.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotObj : MonoBehaviour
+{
+    public float rotSpeed = 60;
+    public float maxRotation = 22.5f;
+
+    private void OnMouseDrag()
+    {
+        if (Input.mousePosition.y >= Screen.height || Input.mousePosition.y <= 0 || Input.mousePosition.x >= Screen.width || Input.mousePosition.x <= 0)
+        {					
+          return;
+        }
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad * 10;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad * 5;
+
+
+        // Debug.Log("Input.GetAxis(MOUSE X)" + Input.GetAxis("Mouse X"));
+        // Debug.Log("Screen Width: " + Screen.width);
+        transform.Rotate(Vector3.forward, -rotX);
+        transform.Rotate(Vector3.right, rotY);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseDrag();
+        };
+    }
+}
